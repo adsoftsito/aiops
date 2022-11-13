26c6e578db5018fc6dffc12024ac3b6151204001
adsoftsito <adsoft@live.com.mx>
2022-11-13 01:09:37 -0600

    Update linear.py

--- linear.py
+++ linear.py
@@ -6,8 +6,23 @@ import os
 # TensorFlow
 import tensorflow as tf
  
-#print(tf.__version__)
+print(tf.__version__)
+
 X = np.arange(-10.0, 10.0, 1e-2)
 np.random.shuffle(X)
 y =  2 * X + 1
 
+train_end = int(0.6 * len(X))
+print (train_end)
+test_start = int(0.8 * len(X))
+print (test_start)
+X_train, y_train = X[:train_end], y[:train_end]
+X_test, y_test = X[test_start:], y[test_start:]
+X_val, y_val = X[train_end:test_start], y[train_end:test_start]
+
+tf.keras.backend.clear_session()
+linear_model = tf.keras.models.Sequential([
+                                           tf.keras.layers.Dense(units=1, input_shape=[1], name='Single')
+                                           ])
+linear_model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.mean_squared_error)
+print(linear_model.summary())
